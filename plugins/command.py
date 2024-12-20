ff8d38d350cc07bfba0da51bb06956b1ddb20700
Fs Botz <fsbotztg@gmail.com>
2024-12-20 05:04:23 +0000

    Update command.py

--- plugins/command.py
+++ plugins/command.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, ChannelInvalid, UsernameInvalid, UsernameNotModified
 import asyncio
 
-    """Handle the /start command."""
+#    """Handle the /start command."""
 @Client.on_message(filters.private & filters.command("start"))
 async def start_command(client, message):
     await message.reply("Bot is in work!")

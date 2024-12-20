2568f2f35b7421926fc7883d61627ace51c2e0db
Fs Botz <fsbotztg@gmail.com>
2024-12-20 08:13:36 +0000

    Update command.py

--- plugins/command.py
+++ plugins/command.py
@@ -12,11 +12,3 @@ async def start_command(client, message):
     await message.reply("Bot is in work!")
 
 
-@Client.on_message(filters.private)  # Filters only private messages
-def handle_message(client, message):
-    user_text = message.text.strip() if message.text else ""
-
-    if user_text.startswith("/"):  # Ignore commands starting with "/"
-        return
-    else:
-        message.reply_text("Bot is in work!")
